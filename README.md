67c37b78b8ab16bae3783b1e80802759f6ed0f4f
christopher.h.castillo@leidos.com <christopher.h.castillo@leidos.com>
2023-07-21 13:43:26 -0400

    bottom nav bar + app bar

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
-# cybersecurity_its_app
+# Cybersecurity ITS App
 
-A new Flutter project.
+The Field Device Security Configuration Tool prototype will provide the ITS Vendor community with a reference design and example implementation of a mobile application and backend cloud service that can be used to guide ITS vendor customers in the secure configuration of ITS equipment. ITS vendors will be able to use the prototype to design their own custom versions of the mobile application and offer the application to their customer base.
+
+Vendor-specific Field Device Security Configuration Tools will be used by ITS Technicians as an aid, providing them with the equipment vendor’s recommended security configuration details for the specific equipment type, connection type and intended use. The Field Device Security Configuration Tool is a mobile application that an ITS Technician can install on either an Android or iPhone device.
 
 ## Getting Started
 
 This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
---
+++ lib/widgets/bottomnavbar.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() =>
+    _BottomNavBarState();
+  }
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget> [
+    Text(
+      'Device',
+      style: optionStyle
+    ),
+    Text(
+      'Help',
+      style: optionStyle
+    ),
+    Text(
+      'Account',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Field Device Security Configuration Tool'),
+        centerTitle: true,
+        titleTextStyle: const TextStyle(fontSize: 18, color: Colors.black),
+      ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _widgetOptions,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.traffic),
+            label: 'Device',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.textsms),
+            label: 'Help',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Account',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
