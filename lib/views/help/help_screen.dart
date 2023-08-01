f2c6b94bb9ebd9e1daaf84712c4ba6668edafc05
christopher.h.castillo@leidos.com <christopher.h.castillo@leidos.com>
2023-08-01 10:07:09 -0400

    added TextField and button. No state yet

--- lib/views/help/help_screen.dart
+++ lib/views/help/help_screen.dart
@@ -12,14 +12,44 @@ class HelpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(label),
-      ),
-      body: ListView(
-          children: const [ 
-            CheckboxWidget()
-          ]),
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+    //GestureDetector allows textfield to come out of focus and hides keyboard when tapping outside.
+    return GestureDetector( 
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(label),
+        ),
+        body: ListView(
+          children: [ 
+            const CheckboxWidget(),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Describe the issue",
+                    alignLabelWithHint: true,
+                ),
+                maxLength: 200,
+                keyboardType: TextInputType.multiline,
+                maxLines: 5,
+              )
+            ),
+            Padding (
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+              
+              //TODO - Disable button if textfield is empty or if checkbox value is null
+              child: ElevatedButton(
+                style: style,
+                onPressed: () {},
+                child: const Text('Submit'),
+              ),
+            )
+          ]
+        ),
+      )
     );
   }
 }
--- lib/views/help/widgets/checkbox.dart
+++ lib/views/help/widgets/checkbox.dart
@@ -30,7 +30,7 @@ class CheckboxWidgetState extends State {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0),
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 32.0),
         child: Column(
           children: List.generate(
             _fields.length,
